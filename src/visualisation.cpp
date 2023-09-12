37a576a393e06468d0d9c4e7d43575d6eed29c80
PositiveBeat <nicoline@finman.dk>
2023-09-12 14:03:04 -0200

    Implemented simple draw_grid function from Stack Overflow

--- src/visualisation.cpp
+++ src/visualisation.cpp
@@ -27,6 +27,7 @@ void Visualisation::update(DiffDrive &vehice)
 
     // Draw points
     window_.clear(sf::Color{180, 220, 240});
+    draw_grid(10, 10);
 
     for (Eigen::Vector3d point : vehice.point_list_earth_)
     {
@@ -37,4 +38,38 @@ void Visualisation::update(DiffDrive &vehice)
     }
 
     window_.display();
+}
+
+void Visualisation::draw_grid(int rows, int cols)
+{
+    /* SOURCE: https://stackoverflow.com/questions/65202726/how-can-i-make-grids-in-sfml-window */
+
+    // initialize values
+    int numLines = rows + cols - 2;
+    sf::VertexArray grid(sf::Lines, 2 * (numLines));
+    window_.setView(window_.getDefaultView());
+    auto size = window_.getView().getSize();
+    float rowH = size.y / rows;
+    float colW = size.x / cols;
+
+    // row separators
+    for (int i = 0; i < rows - 1; i++)
+    {
+        int r = i + 1;
+        float rowY = rowH * r;
+        grid[i * 2].position = {0, rowY};
+        grid[i * 2 + 1].position = {size.x, rowY};
+    }
+
+    // column separators
+    for (int i = rows - 1; i < numLines; i++)
+    {
+        int c = i - rows + 2;
+        float colX = colW * c;
+        grid[i * 2].position = {colX, 0};
+        grid[i * 2 + 1].position = {colX, size.y};
+    }
+
+    // draw it
+    window_.draw(grid);
 }

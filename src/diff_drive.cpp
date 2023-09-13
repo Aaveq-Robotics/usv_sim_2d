e42dcb0868056a5bf58a34786cd1913770d82295
PositiveBeat <nicoline@finman.dk>
2023-09-13 14:32:35 -0200

    Scaled down test USV to more reasonable size

--- src/diff_drive.cpp
+++ src/diff_drive.cpp
@@ -4,9 +4,9 @@ DiffDrive::DiffDrive()
 {
     set_initial_condition({0.0, 0.0, 0.0, 0.0, 0.0, 0.0});
 
-    point_list_body_.push_back({10.0, 100.0, 0.0, 0.0}); // {m, x, y, z}
-    point_list_body_.push_back({15.0, 0.0, 25.0, 0.0});
-    point_list_body_.push_back({15.0, 0.0, -25.0, 0.0});
+    point_list_body_.push_back({0.25, 0.5, 0.0, 0.0}); // {m, x, y, z}
+    point_list_body_.push_back({0.25, 0.0, 0.1, 0.0});
+    point_list_body_.push_back({0.25, 0.0, -0.1, 0.0});
 
     mass_ = sum_mass(point_list_body_);
     point_list_body_ = recompute_relative_to_origin(point_list_body_);
